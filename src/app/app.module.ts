28f89bf43b3fc97b7904d86f66563dd4a4ffb1c6
alexandresantosm <amenezes031@gmail.com>
2021-03-13 10:51:58 -0300

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmeComponent } from 'src/filme/filme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/filme/filme.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'filme',
+    template: `
+      <h2>Catálogo de Filmes</h2>
+      <label>Filme: Avatar</label>
+      <br>
+      <label>Publicação: 2009</label>
+    `
+  }
+)
+export class FilmeComponent {
+
+}
